b985707ea2030b880e4bfa6c56c8f1961abf9c7d
Gramat <mateuszgraczykowski@gmail.com>
2019-02-02 17:47:55 +0100

    index

--- src/main/java/com/gramat/myapp/controller/MainController.java
+++ src/main/java/com/gramat/myapp/controller/MainController.java
@@ -1,17 +1,10 @@
 package com.gramat.myapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-	
-	@RequestMapping("/")
-    @ResponseBody
-    public String home() {
-      return "index";
-    }
 	
 	@RequestMapping("/projects")
 	public String showProjects() {
